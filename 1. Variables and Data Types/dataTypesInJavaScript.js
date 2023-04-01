5c3181549adb0f509c98939538a91359e8a6cb09
Dimitar Iliev <117073615+Dimitar-Iliev7@users.noreply.github.com>
2023-04-01 12:51:37 +0300

    Add files via upload

---
+++ 1. Variables and Data Types/dataTypesInJavaScript.js
@@ -0,0 +1,70 @@
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+// 1. An Object
+// 2. An Array
+// 3. A date
+
+// Numbers:
+let age = 50;
+let numLegs = 2;
+let amount = 200;
+let length = 5;
+let width = 7.1;
+let tires = 4;
+let tiresOfBike = 2;
+let numOfPets = 3;
+
+// Strings:
+let color = "black";
+let name = "John";
+let lastName = "Doe";
+let nameOfPet = "Rex";
+let city = "New York";
+let country = "Canada";
+let carModel = "Tesla";
+let teslaOwner = "Elon Musk";
+
+// Booleans:
+let x = true;
+let y = false;
+
+//Object:
+const person = {
+  firstName: "Michael",
+  lastName: "Scott",
+  series: "The Office",
+  jobTitle: "Regional Manager",
+  age: 50,
+  email: "michael@scott.com",
+  yearsOfWork: 12,
+};
+
+// Array Objects:
+const carBrands = ["BMW", "Mercedes", "Opel"];
+const friendsCast = ["Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"];
+const dogBreeds = ["Afghan Hound", "Akita", "Alaskan Malamute"];
+const years = [1994, 2002, 2020, 2023, 1989, 1546, 2080, 2012, 2018, 2019];
+
+// Date Object
+const date = new Date("2023-04-01");
+
+// Challenges:
+
+// 1. Create a variable my_num and assign it any number.
+let my_num = 23;
+
+// 2. Create a variable my_bool and assign it any boolean.
+const my_bool = true;
+
+// 3. Create a variable my_str and assign it any string.
+let my_str = "Apple fan";
